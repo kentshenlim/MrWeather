5578c1ee14511cc0d0683d513035b823f88afcd5
kentshenlim <kentshen_lim24@hotmail.com>
2023-09-01 12:11:01 +0100

    Remove default stylings

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { styled } from 'styled-components';
 import './App.css';
+
+const Wrapper = styled.div`
+  border: solid red 3px;
+  width: 100%;
+  height: 100%;
+`;
 
 function App() {
   return (
-    <div>
+    <Wrapper>
       <h1>Hello MrWeather</h1>
-    </div>
+    </Wrapper>
   );
 }
 
