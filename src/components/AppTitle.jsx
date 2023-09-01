e24de994d7db5616a3752f7b770340cab8b14d36
kentshenlim <kentshen_lim24@hotmail.com>
2023-09-01 21:25:24 +0100

    Reduce styled components

--- src/components/AppTitle.jsx
+++ src/components/AppTitle.jsx
@@ -7,30 +7,22 @@ const Wrapper = styled.div`
   flex-direction: column;
   align-items: center;
   width: 12rem;
-`;
-
-const TitleText = styled.h1`
-  margin: 0;
-  font-size: ${fontSize.largest};
-`;
-
-const TitleAnchor = styled.a``;
-
-const TitleImg = styled.img`
-  width: 80%;
-  margin-top: -0.5rem;
+  > h1 {
+    margin: 0;
+    font-size: ${fontSize.largest};
+  }
+  > a > img {
+    width: 80%;
+  }
 `;
 
 export default function AppTitle() {
   return (
     <Wrapper>
-      <TitleText>MrWeather</TitleText>
-      <TitleAnchor
-        href="https://github.com/kentshenlim/MrWeather"
-        target="_blank"
-      >
-        <TitleImg src={mrWeather} alt="MrWeather Icon" />
-      </TitleAnchor>
+      <h1>MrWeather</h1>
+      <a href="https://github.com/kentshenlim/MrWeather" target="_blank">
+        <img src={mrWeather} alt="MrWeather Icon" />
+      </a>
     </Wrapper>
   );
 }
