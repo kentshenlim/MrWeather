89326add3b89086d42423c7dd98db24a7e26b658
kentshenlim <kentshen_lim24@hotmail.com>
2023-09-21 15:15:26 +0800

    Refactor 3d load function

--- src/components/Model2.jsx
+++ src/components/Model2.jsx
@@ -1,8 +1,6 @@
 import { styled } from 'styled-components';
-import { useEffect } from 'react';
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'; // Allow interaction
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'; // Load gltf file (3D model)
+import { useEffect, useRef } from 'react';
+import loadModel from '../utils/loadModel';
 import model from '../assets/model/fireInTheSky/scene.gltf';
 
 const Wrapper = styled.div`
@@ -20,72 +18,22 @@ const Wrapper = styled.div`
 `;
 
 export default function Model2() {
+  const wrapperRef = useRef(null);
+
   useEffect(() => {
-    // Init
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(
-      100,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    );
-    let object; // 3D object
-    let controls; // OrbitControls
-    const loader = new GLTFLoader(); // Loader for gltf
-
-    // Load the file
-    loader.load(
-      model,
-      function (gltf) {
-        // If the file is loaded, add it to the scene
-        object = gltf.scene;
-        scene.add(object); // Add this loaded object into scene to be rendered later
-      },
-      function (xhr) {
-        // While it is loading, log the progress
-        console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
-      },
-      function (error) {
-        // If there is an error, log it
-        console.error(error);
-      }
-    );
-
-    // Instantiate a new renderer and set its size
-    const renderer = new THREE.WebGLRenderer({ alpha: true }); // Alpha: true allows for transparent background
-    renderer.setSize(window.innerWidth / 3, window.innerHeight / 3);
-
-    // Add the renderer to the DOM
-
-    // Camera and light
-    camera.position.z = 1.5;
-    const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
-    topLight.position.set(500, 500, 500); // top-left-ish
-    topLight.castShadow = true;
-    scene.add(topLight);
-    const ambientLight = new THREE.AmbientLight(0x333333, 1);
-    scene.add(ambientLight);
-
-    // Allows orbit controls
-    controls = new OrbitControls(camera, renderer.domElement);
-
-    // Render the scene
-    function animate() {
-      requestAnimationFrame(animate);
-      renderer.render(scene, camera);
-    }
-
-    // Camera and window responsive
-    window.addEventListener('resize', function () {
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
-      renderer.setSize(window.innerWidth / 3, window.innerHeight / 3);
+    const container = wrapperRef.current;
+    const model3D = loadModel({
+      model: model,
+      rendererWidth: window.innerWidth / 3,
+      rendererHeight: window.innerHeight / 3,
+      cameraPositionZ: 1.5,
     });
-
-    // Start the 3D rendering
-    animate();
-    document.getElementById('container3D').appendChild(renderer.domElement);
+    container.appendChild(model3D.domElement);
+    return () => {
+      container.removeChild(model3D.domElement);
+      model3D.cleanUp();
+    };
   }, []);
 
-  return <Wrapper id="container3D"></Wrapper>;
+  return <Wrapper id="container3D" ref={wrapperRef}></Wrapper>;
 }
---
+++ src/utils/LoadModel.js
@@ -0,0 +1,76 @@
+// Modified based on https://github.com/gjmolter/web-3dmodel-threejs
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'; // Allow interaction
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'; // Load gltf file (3D model)
+
+export default function loadModel({ model, rendererWidth, rendererHeight, cameraPositionZ }) { // Init
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      100,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    let object; // 3D object
+    let controls; // OrbitControls
+    const loader = new GLTFLoader(); // Loader for gltf
+
+    // Load the file
+    loader.load(
+      model,
+      function (gltf) {
+        // If the file is loaded, add it to the scene
+        object = gltf.scene;
+        scene.add(object); // Add this loaded object into scene to be rendered later
+      },
+      function (xhr) {
+        // While it is loading, log the progress
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+      },
+      function (error) {
+        // If there is an error, log it
+        console.error(error);
+      }
+    );
+
+    // Instantiate a new renderer and set its size
+    const renderer = new THREE.WebGLRenderer({ alpha: true }); // Alpha: true allows for transparent background
+    renderer.setSize(rendererWidth, rendererHeight);
+
+    // Camera and light
+    camera.position.z = cameraPositionZ;
+    const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
+    topLight.position.set(500, 500, 500); // top-left-ish
+    topLight.castShadow = true;
+    scene.add(topLight);
+    const ambientLight = new THREE.AmbientLight(0x333333, 1);
+    scene.add(ambientLight);
+
+    // Allows orbit controls
+    controls = new OrbitControls(camera, renderer.domElement);
+
+    // Render the scene
+    function animate() {
+      requestAnimationFrame(animate);
+      renderer.render(scene, camera);
+    }
+
+    // Camera and window responsive
+    function handleResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(rendererHeight, rendererWidth);
+    }
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup function for React effect
+    function cleanUp() {
+        renderer.dispose();
+        window.removeEventListener('resize', handleResize);
+    }
+
+    // Start the 3D rendering
+    animate();
+
+    return { domElement: renderer.domElement, cleanUp: cleanUp };
+}
