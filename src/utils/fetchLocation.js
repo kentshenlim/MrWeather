8cae8f56002f5315a6134060808e0daaf612fbc5
kentshenlim <kentshen_lim24@hotmail.com>
2023-09-24 19:45:51 +0800

    Fix url

--- src/utils/fetchLocation.js
+++ src/utils/fetchLocation.js
@@ -1,10 +1,11 @@
 async function fetchLocation(text) {
-    const base_url = 'http://api.weatherapi.com/v1/search.json';
-    const key = import.meta.env.VITE_WEATHER_API_KEY;
-    const url = base_url + '?' + 'key=' + key + `&q=${text}`;
-    const res = await fetch(url);
-    const goodRes = await res.json();
-    return goodRes;
+  const base_url = 'https://api.weatherapi.com/v1/search.json';
+//   const key = import.meta.env.VITE_WEATHER_API_KEY;
+  const key = '806b03d5d46741248aa123547232109'; // This API is free
+  const url = base_url + '?' + 'key=' + key + `&q=${text}`;
+  const res = await fetch(url);
+  const goodRes = await res.json();
+  return goodRes;
 }
 
 export default fetchLocation
