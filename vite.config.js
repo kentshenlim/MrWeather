a9b9a6399ddb776eafff26e4b46ff073a96d12c6
kentshenlim <kentshen_lim24@hotmail.com>
2023-09-25 14:38:25 +0800

    Change dir

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {minify: false},
   assetsInclude: ['**/*.gltf', '**/*.glb'],
   plugins: [react()],
   test: {
